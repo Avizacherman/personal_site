c36c77c3424b7aff3475ed81791cf05dabec3df8
Avi Zacherman <avizacherman@gmail.com>
2016-01-20 15:13:55 -0500

    getting better at this style stuff

--- public/scripts/main.es6.js
+++ public/scripts/main.es6.js
@@ -1,156 +1,13 @@
-//The purpose of this file is to provide a level of functionality to my personal website. The code is intentionally not DRY, but rather written as simple as possible. 
+//The purpose of this file is to provide a level of functionality to my personal website. The code is intentionally not DRY, but rather written as simple as possible.
 
 $(document).ready(function() {
-	
-	//preload animated GIFs
-	var $ticTac = $('<img>').attr('src', 'img/tictactoe.gif').attr('id', 'ticTacImg').addClass('detail-pic');
-	var $wiki = $('<img>').attr('src', 'img/wiki.gif').attr('id', 'wikiImg').addClass('detail-pic');
-	var $jizo = $('<img>').attr('src', 'img/jizo.gif').attr('id', 'jizoImg').addClass('detail-pic');
-	var $phoodography = $('<img>').attr('src', 'img/phoodography.gif').attr('id', 'phoodographyImg').addClass('detail-pic')
 
-	var beer = 'img/beer.png'
-	var boardGames = 'img/boardgames.png'
-	var cooking = 'img/cooking.png'
-	var exploring = 'img/explore.png'
-	var ramen = 'img/ramen.png'
+
 
 	var $holder = $('#image-holder');
 	var $linker = $('#linker');
 
-//Animated Gif Rotator
-	var targetArray = [$ticTac, $wiki, $jizo, $phoodography];
-	var hobbyArray = [beer, boardGames, cooking, exploring, ramen]
 
-	var linkArray = ["http://avizacherman.github.io", "http://104.131.4.248:1337", "https://jizomap.herokuapp.com", "https://phoodography.herokuapp.com"]
-
-	var hobbyItemArray = [$('#beer'), $('#board-games'), $('#cooking'), $('#exploring'), $('#ramen')]	
-	var projectItemArray = [$('#ticTacToe'), $('#wiki'), $('#jizo'), $('#phoodography')];
-	
-
-	var projectIndex = 1;
-	var hobbyIndex = 1;
-
-	var hobbyRotation = setInterval(() => {
-		$('#hobby-image').attr('src', hobbyArray[hobbyIndex])
-			
-			hobbyItemArray.forEach((item, index)=> {
-			
-			if(hobbyIndex === index){
-				item.addClass('highlighted')
-			} else {
-				item.removeClass('highlighted')
-			}
-		})
-
-			hobbyIndex++
-
-		if(hobbyIndex === hobbyArray.length){
-			hobbyIndex = 0
-		}
-
-	}, 3500)
-
-	var projectRotation = setInterval(function(){
-		$holder.children().first().remove()
-
-		$holder.append(targetArray[projectIndex])
-		$linker.attr('href', linkArray[projectIndex]).text(linkArray[projectIndex])
-		projectItemArray.forEach((item, index)=> {
-			if(projectIndex === index){
-				item.addClass('highlighted')
-			} else {
-				item.removeClass('highlighted')
-			}
-
-		})
-		projectIndex++
-
-		if(projectIndex === targetArray.length){
-			projectIndex = 0
-		}
-	}, 3500);
-
-	
-//Click events for animated GIFs
-	$('#ticTacToe').click(function(e){
-		e.stopPropagation()
-		$holder.children().first().remove()
-		$holder.append($ticTac)
-		$linker.attr('href', linkArray[0]).text(linkArray[0])
-		$('#projects').children().removeClass('highlighted')
-		$(this).addClass('highlighted')
-		clearInterval(projectRotation)
-	})
-
-	$('#wiki').click(function(e){
-		e.stopPropagation()
-		$holder.children().first().remove()
-		$holder.append($wiki)
-		$linker.attr('href', linkArray[1]).text(linkArray[1])
-		$('#projects').children().removeClass('highlighted')
-		$(this).addClass('highlighted')		
-		clearInterval(projectRotation)
-	})
-
-		$('#jizo').click(function(e){
-		e.stopPropagation()
-		$holder.children().first().remove()
-		$holder.append($jizo)
-		$linker.attr('href', linkArray[2]).text(linkArray[2])
-		$('#projects').children().removeClass('highlighted')		
-		$(this).addClass('highlighted')
-		clearInterval(projectRotation)
-	})
-
-	$('#phoodography').click(function(e){
-		e.stopPropagation()
-		$holder.children().first().remove()
-		$holder.append($phoodography)
-		$linker.attr('href', linkArray[3]).text(linkArray[3])
-		$('#projects').children().removeClass('highlighted')		
-		$(this).addClass('highlighted')
-		clearInterval(projectRotation)
-	})	
-
-	$('#beer').click(function(e){
-		e.stopPropagation()
-		$('#hobby-image').attr('src', hobbyArray[0])
-		$('#hobbies').children().removeClass('highlighted')
-		$(this).addClass('highlighted')
-		clearInterval(hobbyRotation)
-	})	
-
-	$('#board-games').click(function(e){
-		e.stopPropagation()
-		$('#hobby-image').attr('src', hobbyArray[1])
-		$('#hobbies').children().removeClass('highlighted')
-		$(this).addClass('highlighted')
-		clearInterval(hobbyRotation)
-	})	
-
-	$('#cooking').click(function(e){
-		e.stopPropagation()
-		$('#hobby-image').attr('src', hobbyArray[2])
-		$('#hobbies').children().removeClass('highlighted')
-		$(this).addClass('highlighted')
-		clearInterval(hobbyRotation)
-	})			
-
-	$('#exploring').click(function(e){
-		e.stopPropagation()
-		$('#hobby-image').attr('src', hobbyArray[3])
-		$('#hobbies').children().removeClass('highlighted')
-		$(this).addClass('highlighted')
-		clearInterval(hobbyRotation)
-	})	
-
-	$('#ramen').click(function(e){
-		e.stopPropagation()
-		$('#hobby-image').attr('src', hobbyArray[4])
-		$('#hobbies').children().removeClass('highlighted')
-		$(this).addClass('highlighted')
-		clearInterval(hobbyRotation)
-	})		
 	//Form Validation
 	$('#contact-form').form({
 		on: 'submit',
@@ -174,13 +31,13 @@ $(document).ready(function() {
 					prompt: 'Please enter a valid e-mail address'
 				}]
 			},
-			subject: 
+			subject:
 			{ identifier: 'subject',
 				rules: [{
 					type: 'empty',
 					prompt: 'Please fill in this field'
 				}]
-			}, 
+			},
 			content: {
 				identifier: 'content',
 				rules: [{
@@ -239,7 +96,7 @@ function slideChange(slideOut, slideIn){
 //DRY Set
 function slideShow(){  			//creates a series of three second intervals, staggered with slide changes
 	var firstSlide = setTimeout(function(){
-			slideChange('one', 'two') 
+			slideChange('one', 'two')
 		}, 3000)
 
 	var secondSlide = setTimeout(function(){
@@ -262,7 +119,7 @@ function slideShow(){  			//creates a series of three second intervals, staggere
 slideShow()
 //Repeating Set
 var slides = setInterval(function(){
-	
+
 	slideShow()
 
 }, 15000)
